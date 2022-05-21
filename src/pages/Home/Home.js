aed17ec3e6aa796769760c776eef35b8dd90151d
morshedulmunna <morshedulmunna1@gmail.com>
2022-05-22 01:12:28 +0600

    Project File Setup

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import useTitle from "../../helper/useTitle";
+
+const Home = () => {
+  useTitle("Alliance Inc");
+  return (
+    <div>
+      <h1>This is Home 🚀</h1>
+    </div>
+  );
+};
+
+export default Home;
